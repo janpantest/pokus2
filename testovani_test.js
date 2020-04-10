c510264d47170afc9068dfa9bf86669ad42c6eb6
janpantest <krchovaak@gmail.com>
2020-04-10 13:07:53 +0200

    .gitignore added + tests modified

--- testovani_test.js
+++ testovani_test.js
@@ -1,36 +1,38 @@
 Feature('testovani');
 
 
-// Scenario('test run', async (I) => {
-//     const assert = require('assert');
-//     I.amOnPage('https://google.cz')
-//     I.waitForElement({xpath: "//input[@name='q']"});
-//     I.seeElement({xpath: "//input[@name='q']"});
-//     I.click({xpath: "//input[@name='q']"});
-//     I.fillField({xpath: "//input[@name='q']"}, "iliteratura");
-//     I.click({xpath: "(//div[@class='sbtc'])[1]"});
-//     I.click({xpath: "//a[./text()='Recenze']"});
-//     I.waitForElement({xpath: "//div/span[./text()='Robb, Alice: Proč sníme?']"})
-//     I.click({xpath: "//div/span[./text()='Robb, Alice: Proč sníme?']"});
-//     let title = await I.grabTitle(); 
-//     console.log(`Current title is ${title}`);
-//     assert.equal(title, 'Robb, Alice: Proč sníme? - iLiteratura.cz');
-//     I.seeInTitle('Proč sníme')
+Scenario('test run', async (I) => {
+    const assert = require('assert');
+    I.amOnPage('https://google.cz')
+    I.waitForElement({xpath: "//input[@name='q']"});
+    I.seeElement({xpath: "//input[@name='q']"});
+    I.click({xpath: "//input[@name='q']"});
+    I.fillField({xpath: "//input[@name='q']"}, "iliteratura");
+    I.click({xpath: "(//div[@class='sbtc'])[1]"});
+    I.click({xpath: "//a[./text()='Recenze']"});
+    I.waitForElement({xpath: "//div/span[./text()='Robb, Alice: Proč sníme?']"})
+    I.click({xpath: "//div/span[./text()='Robb, Alice: Proč sníme?']"});
+    let title = await I.grabTitle(); 
+    console.log(`Current title is ${title}`);
+    assert.equal(title, 'Robb, Alice: Proč sníme? - iLiteratura.cz');
+    I.seeInTitle('Proč sníme')
 
-// })
+})
 
-// Scenario('test run dva', async (I) => {
-//     const assert = require('assert');
-//     I.amOnPage('https://seznam.cz')
-//     I.seeInTitle('Seznam');
-//     I.waitForElement({xpath: "(//input[@name='q'])[3]"});
-//     I.fillField({xpath: "(//input[@name='q'])[3]"}, 'iliteratura');
-//     I.click({xpath: "//button[@class='search-form__button button button--submit button--with-input']"})
-//     I.waitForElement({xpath: "//a[./text()='Seznam']"})
-//     I.click({xpath: "//span[./text()='Recenze']"})
-//     I.waitForElement({xpath: "//span[./text()='Robb, Alice: Proč sníme?']"})
-//     I.click({xpath: "//span[./text()='Robb, Alice: Proč sníme?']"})
-//     I.seeInTitle('Robb')
+Scenario('test run dva', async (I) => {
+    const assert = require('assert');
+    I.amOnPage('https://seznam.cz')
+    I.seeInTitle('Seznam');
+    I.waitForElement({xpath: "(//input[@name='q'])[3]"});
+    I.fillField({xpath: "(//input[@name='q'])[3]"}, 'iliteratura');
+    I.click({xpath: "//button[@class='search-form__button button button--submit button--with-input']"})
+    I.waitForElement({xpath: "//a[./text()='Seznam']"})
+    I.waitForClickable({xpath: "//span[./text()='Recenze']"})
+    I.click({xpath: "//span[./text()='Recenze']"})
+    I.waitForClickable({xpath: "//span[./text()='Robb, Alice: Proč sníme?']"})
+    I.click({xpath: "//span[./text()='Robb, Alice: Proč sníme?']"})
+    I.seeInTitle('Robb')
+})
 
 
 Scenario('another test', async(I) => {
